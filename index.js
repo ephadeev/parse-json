8d7fe56e475fe6bd2e3c218d946eba28f40b2a8b
ephadeev <itrex1991@hotmail.com>
2021-06-21 18:38:20 +0300

    v3

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 const fs = require('fs');
 
 /**
- * Серверы из списка который скидывал Женя Капешко (мануально удалены серверы которых нет Entwicklungumgebung
- * и в Hambach
- * */
-let neededServersWithoutVersion = fs
-    .readFileSync('S00U08_servers.txt', 'utf8')
-    .split('\n'); // neededServersWithoutVersion.length = 315 шт
-
-// console.log(neededServersWithoutVersion);
-
-
-/**
  * looksce.txt лог выведенный из OutsideView. Сделан был поиск файлов начинающихся на S* в SCE всех продуктов в PG3.
  * Мануально удалены несколько серверов из 98 продукта.
  * sces  - массив массивов, в каждом первый элемент - имя сервака в pg3 (c версией), есть копии (старые и новые версии).
  * */
-let sces = fs
+/*let sces = fs
     .readFileSync('looksce.txt', 'utf8')
     .split('\n')
     .map(line => line.split(' '))
@@ -25,7 +14,7 @@ let sces = fs
         if (sce[0].charAt(0) === 'S' && sce[0].charAt(3) === 'S') {
             return sce;
         }
-    });
+    });*/
 
 // console.log(sces);// [ [], [], [] ]   [][0] - имя с версией, [][6] - 'NCNC' или 'N---'
 
@@ -33,37 +22,34 @@ let sces = fs
 /**
  * servers - JSON выведенный из Entwicklungumgebung Эрнста. Таблица plus-server.
  * */
-let servers = require('./plus_server'); // [{}, {}, {}, ...]
-
-let sqlStrings = servers
-    .map(server => {
-        let serverNameWithoutVersion = server['server'].substring(0, 6);
-
-        neededServersWithoutVersion.forEach(elem => {
-            // console.log('elem: ', elem);
-            // console.log('serverNameWithoutVersion', serverNameWithoutVersion);
-            if (elem === serverNameWithoutVersion) {
-                // console.log(elem === serverNameWithoutVersion);
-                // console.log(server);
-                return server;
-            }
-        });
-
-        console.log(findCustom(neededServersWithoutVersion, serverNameWithoutVersion)); // почему-то из массива neededServersWithoutVersion только последнюю запись берет??
-
-        // return neededServersWithoutVersion.includes(server['server'].substring(0, 6));
-        /*if (
-            server['server'].charAt(0) === 'S' &&
-            server.server.charAt(3) === 'S'
-        ) {
-            console.log(server['server'], neededServersWithoutVersion.includes(server['server'].substring(0, 6)), server['server'].substring(0, 6));
-            return server; // почему только последний оставил?
-        }*/
-    }); // [{}, {}, {}]
-
-// console.log(sqlStrings);
-
-
-function findCustom(arr, el) {
-    return arr.filter(element => element === el);
+async function getPlusServerData(product) {
+    let servers = await require('./plus_server');
+    return servers.filter(server => server['server'].substring(1, 3) === `${product}` && server['server'].substring(0, 1) === 'S' && server['server'].substring(3, 4) === 'S');
 }
+
+
+/**
+ * Серверы из списка который скидывал Женя Капешко (мануально удалены серверы которых нет Entwicklungumgebung
+ * и в Hambach
+ * */
+async function getNeededServers(product) {
+    let neededServersWithoutVersion = await fs
+        .readFileSync('S00U08_servers.txt', 'utf8')
+        .split('\n'); // neededServersWithoutVersion.length = 315 шт
+    return neededServersWithoutVersion.filter(neededServers => neededServers.substring(1, 3) === `${product}` && neededServers.substring(0, 1) === 'S' && neededServers.substring(3, 4) === 'S');
+}
+
+Promise.all([getPlusServerData(35), getNeededServers(35)]).then(result => {
+    // result[0]; // json из инеос со всей кашей
+    // result[1]; // нужные нам серваки без расширения
+
+    result[1].forEach(neededServer => {
+        result[0].forEach(server => {
+            let newVersion = +server['server'].substring(6, 8) + 1;
+            let serverName = server['server'].substring(0, 6);
+            if (serverName === neededServer.replace(/\r/g, '')) {
+                console.log(`UPDATE plus_ineos.plus_server SET server = \"${server['server'].substring(0, 6)}${newVersion > 9 ? `${newVersion}` : `0${newVersion}`}\", server_path = \"${server['server_path'].substring(0, 22)}${newVersion > 9 ? `${newVersion}` : `0${newVersion}`}\", compile_status = \"NEW\" WHERE plus_server.server = \"${server['server']}\";`);
+            }
+        })
+    });
+});
