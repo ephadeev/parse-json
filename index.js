3024b73aead16426a88c350bb5c77a1e08a703d9
ephadeev <itrex1991@hotmail.com>
2021-06-22 20:10:40 +0300

    v4

--- index.js
+++ index.js
@@ -5,18 +5,14 @@ const fs = require('fs');
  * Мануально удалены несколько серверов из 98 продукта.
  * sces  - массив массивов, в каждом первый элемент - имя сервака в pg3 (c версией), есть копии (старые и новые версии).
  * */
-/*let sces = fs
-    .readFileSync('looksce.txt', 'utf8')
-    .split('\n')
-    .map(line => line.split(' '))
-    .filter(sce => {
-        // фильтруем чтобы остались только сервера:
-        if (sce[0].charAt(0) === 'S' && sce[0].charAt(3) === 'S') {
-            return sce;
-        }
-    });*/
+async function getSceData() {
+    let sces = await fs
+        .readFileSync('looksce.txt', 'utf8')
+        .split('\n')
+        .map(line => line.split(' '));
 
-// console.log(sces);// [ [], [], [] ]   [][0] - имя с версией, [][6] - 'NCNC' или 'N---'
+    return sces.filter((element, index, array) => element[0].charAt(0) === 'S' && element[0].charAt(3) === 'S');
+}
 
 
 /**
@@ -24,7 +20,7 @@ const fs = require('fs');
  * */
 async function getPlusServerData(product) {
     let servers = await require('./plus_server');
-    return servers.filter(server => server['server'].substring(1, 3) === `${product}` && server['server'].substring(0, 1) === 'S' && server['server'].substring(3, 4) === 'S');
+    return servers.filter(server => server['server'].substring(1, 3) === `${product > 9 ? `${product}` : `0${product}`}` && server['server'].substring(0, 1) === 'S' && server['server'].substring(3, 4) === 'S');
 }
 
 
@@ -36,10 +32,10 @@ async function getNeededServers(product) {
     let neededServersWithoutVersion = await fs
         .readFileSync('S00U08_servers.txt', 'utf8')
         .split('\n'); // neededServersWithoutVersion.length = 315 шт
-    return neededServersWithoutVersion.filter(neededServers => neededServers.substring(1, 3) === `${product}` && neededServers.substring(0, 1) === 'S' && neededServers.substring(3, 4) === 'S');
+    return neededServersWithoutVersion.filter(neededServers => neededServers.substring(1, 3) === `${product > 9 ? `${product}` : `0${product}`}` && neededServers.substring(0, 1) === 'S' && neededServers.substring(3, 4) === 'S');
 }
 
-Promise.all([getPlusServerData(35), getNeededServers(35)]).then(result => {
+Promise.all([getPlusServerData(35), getNeededServers(35), getSceData()]).then(result => {
     // result[0]; // json из инеос со всей кашей
     // result[1]; // нужные нам серваки без расширения
 
@@ -51,5 +47,13 @@ Promise.all([getPlusServerData(35), getNeededServers(35)]).then(result => {
                 console.log(`UPDATE plus_ineos.plus_server SET server = \"${server['server'].substring(0, 6)}${newVersion > 9 ? `${newVersion}` : `0${newVersion}`}\", server_path = \"${server['server_path'].substring(0, 22)}${newVersion > 9 ? `${newVersion}` : `0${newVersion}`}\", compile_status = \"NEW\" WHERE plus_server.server = \"${server['server']}\";`);
             }
         })
+    });
+
+    result[2].forEach((element, index, array) => {
+        if (index > 1) {
+            if (element[0].substring(0, 6) === array[index - 1][0].substring(0, 6) && element[0].substring(0, 6) === array[index - 2][0].substring(0, 6)) {
+                // console.log(element[0], ' - ', array[index - 1][0], ' - ', array[index - 2][0]);
+            }
+        }
     });
 });
